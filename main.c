abd8ff702617d5a9a103c65e57d69b44f784eaa4
vaninadimitrova51 <vaninadimitrova51@abv.bg>
2018-02-23 19:27:01 +0200

    date2

--- main.c
+++ main.c
@@ -1,53 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
 
 int main()
 {
 
-    int firstNumber;
-    int secondNumber;
-    int thirdNumber;
-    int day, month, year;
-    int ndays;
-    int nMonths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int day;
+    int month;
+    int year;
+    int newDay;
+    int numberOfDaysInMonth;
+
+    printf("Enter day\n");
+    scanf("%d", &day);
+
+    printf("Enter month\n");
+    scanf("%d", &month);
+
+    printf("Enter year\n");
+    scanf("%d", &year);
+
+    printf("The date is %d/%d/%d\n\n", day, month, year);
 
 
-    printf("Enter the day\n");
-    scanf("%d", &firstNumber);
+    if(year % 4 == 0){
+        printf("\nVisokosna\n(ne znam kak e na angliiski ne me sydi :D)\n\n");
+        if(month == 2){
+            if(day < 30){
 
-    printf("Enter the month\n");
-    scanf("%d", &secondNumber);
+                void Months();
 
-    printf("Enter the year\n");
-    scanf("%d", &thirdNumber);
+                numberOfDaysInMonth = month;
+                newDay = day - 1;
 
-    printf("The date is: %d/%d/%d\n", firstNumber, secondNumber, thirdNumber);
+                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
 
-    ndays = nMonths[secondNumber + 1];
-
-    if(secondNumber == 2){
-        if(thirdNumber % 100 == 0){
-            if(thirdNumber % 400 == 0){
-                ndays = 28;
+            }else if(day > 29){
+                printf("\nThis date does not exist\n");
             }
-        }else if(thirdNumber % 4 == 0){
-            ndays = 29;
-        }
-        day = firstNumber - 1;
-        month = secondNumber;
-        year = thirdNumber;
-        if(day > ndays){
-            month++;
-        }
-        if(month > 12){
-            month = 1;
-            year++;
-        }
-    }
+    }else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            if(day < 32){
+                numberOfDaysInMonth = month;
+                newDay = day - 1;
 
-    printf("The date before: %d/%d/%d", day, month, year);
+                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
+            }else if(day > 31){
+                printf("\nThis date does not exist\n");
+            }
+        }else if(month == 2 || month == 4 || month == 6 || month == 9 || month == 11){
+            if(day < 31){
+                numberOfDaysInMonth = month;
+                newDay = day - 1;
+
+                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
+            }else if(day > 30){
+                printf("\nThis date does not exist\n");
+            }
+        }
+
+
+
+    }else if(year % 100 != 0){
+        printf("\nNe e visokosna\n(ne znam kak e na angliiski ne me sydi :D)\n");
+        if(month == 2){
+            if(day < 29){
+
+                numberOfDaysInMonth = month;
+                newDay = day - 1;
+
+                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
+
+            }else if(day > 28){
+            printf("\nThis date does not exist\n");
+            }
+        }else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            if(day < 32){
+                numberOfDaysInMonth = month;
+                newDay = day - 1;
+
+                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
+            }else if(day > 31){
+                printf("\nThis date does not exist\n");
+            }
+        }else if(month == 2 || month == 4 || month == 6 || month == 9 || month == 11){
+            if(day < 31){
+                numberOfDaysInMonth = month;
+                newDay = day - 1;
+
+                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
+            }else if(day > 30){
+                printf("\nThis date does not exist\n");
+            }
+        }
+
+
+    }
 
 
     return 0;
+}
+
+void Months (int* numberOfDaysInMonth){
+
+    int January[31] = {1};
+    int February[28] = {2};
+    int March[31] = {3};
+    int April[30] = {4};
+    int May[31] = {5};
+    int June[30] = {6};
+    int July[31] = {7};
+    int August[31] = {8};
+    int September[30] = {9};
+    int October[31] = {10};
+    int November[30] = {11};
+    int December[31] = {12};
+
+
+
+    return;
 }
